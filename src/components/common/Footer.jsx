cafc4efad9971259d502de5e03e12960d5f47e12
gabriel-lunesu <g.lunesu@outlook.com>
2024-10-10 23:28:40 +0200

    blog in footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -21,6 +21,7 @@ export default function Footer() {
                   <ul className="text-lg flex items-center justify-center flex-col gap-7 py-16 mb-10">
                     <li><a href="#" class="text-white hover:text-white">Algemene Voorwaarden</a></li>
                     <li><a href="#" class="text-white hover:text-white">Privacy Policy</a></li>
+                    <li><a href="/blog" class="text-white hover:text-white">Blog</a></li>
                   </ul>
                   <div class="flex space-x-10 justify-center items-center mb-14">
                       <a href="#" class="block  text-white transition-all duration-500 hover:text-indigo-600 ">
