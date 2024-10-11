313331ae14be901b36511f16410138c89503e290
gabriel-lunesu <g.lunesu@outlook.com>
2024-10-11 15:09:45 +0200

    fixed hero

--- src/components/blog/Hero.jsx
+++ src/components/blog/Hero.jsx
@@ -4,7 +4,7 @@ import { SparklesCore } from "@/components/ui/sparkles";
 
 export function Hero()  {
   return (
-    <div className="h-[20rem] bg-purple-900 flex flex-col items-center justify-center overflow-hidden rounded-md">
+    <div className="mt-24 bg-purple-900 flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1 className="text-4xl font-bold text-white">Onze Blog</h1>
     </div>
   );
