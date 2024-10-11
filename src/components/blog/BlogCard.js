01602d7a5323480ba7a93386169d7dd2bbdc304a
gabriel-lunesu <g.lunesu@outlook.com>
2024-10-11 14:48:19 +0200

    updated blog page

--- src/components/blog/BlogCard.js
+++ src/components/blog/BlogCard.js
@@ -1,23 +1,20 @@
-'use client';
-
+import React from 'react';
 import Link from 'next/link';
-import React from "react";
 
 export default function BlogCard({ page }) {
   const title = page.properties.Title.title[0]?.plain_text || 'Untitled';
   const slug = page.properties.slug?.rich_text[0]?.plain_text || page.id;
 
   return (
-    <Link href={`/blog/${slug}`}>
-      <div className="group relative overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
-        <div className="bg-white dark:bg-gray-800 p-6">
-          <h2 className="text-2xl font-bold mb-2 text-gray-800 dark:text-white group-hover:text-blue-600 dark:group-hover:text-blue-400 transition-colors duration-300">
+    <Link href={`/blog/${slug}`} className="block">
+      <div className="bg-purple-100 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
+        <div className="p-6">
+          <h2 className="text-2xl font-bold text-purple-800 hover:text-purple-600 transition-colors duration-300">
             {title}
           </h2>
-          <div className="absolute bottom-0 left-0 right-0 h-1 bg-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></div>
         </div>
+        <div className="h-1 bg-gradient-to-r from-purple-400 to-pink-500"></div>
       </div>
     </Link>
   );
 }
-
--- src/components/blog/BlogList.js
+++ src/components/blog/BlogList.js
@@ -5,7 +5,7 @@ export default async function BlogList(){
   const pages = await fetchPages();
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mt-20 mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8 text-center text-gray-800 dark:text-white">Blog Posts</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {pages.results.map((page) => (
