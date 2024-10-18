3fd797dd097333a84f3075ad12d1942102ac2942
gabriel-lunesu <g.lunesu@outlook.com>
2024-10-18 18:59:03 +0200

    Pika marketing pics added

--- src/components/common/HeroParallax.jsx
+++ src/components/common/HeroParallax.jsx
@@ -50,40 +50,40 @@ export const products = [
   },
   // vanaf hier moet je nieuwe toevoegen
   {
-    title: "TROA Makelaardij",
-    link: "https://troa.nl/",
+    title: "Pika Marketing",
+    link: "https://pikamarketing.com/",
     thumbnail:
-      "https://i.ibb.co/8mP1qbz/Screenshot-2024-10-16-at-22-54-02.png",
+      "https://i.ibb.co/sw0YqQ8/Screenshot-2024-10-18-at-18-47-31.png",
   },
   {
-    title: "Heki Made",
-    link: "https://hekimade.com/",
+    title: "Lead Master",
+    link: "https://leadmaster.com/",
     thumbnail:
-      "https://i.ibb.co/tsQLY0b/Screenshot-2024-10-16-at-22-59-20.png",
+      "https://i.ibb.co/n7pQZzw/Screenshot-2024-10-18-at-18-51-34.png",
   },
   {
-    title: "Synchronique Agency",
-    link: "https://hekimade.com/",
+    title: "Demogency",
+    link: "https://demogency.com/",
     thumbnail:
-      "https://i.ibb.co/tsQLY0b/Screenshot-2024-10-16-at-22-59-20.png",
+      "https://i.ibb.co/py2cT0d/Screenshot-2024-10-18-at-18-53-17.png",
   },
   {
-    title: "Thuistekoop",
-    link: "https://hekimade.com/",
+    title: "Saranda Schoonheidshuis Academie",
+    link: "https://saranda-maquillage-permanent.ch/",
     thumbnail:
-      "https://i.ibb.co/LPyMJ4J/Screenshot-2024-10-16-at-23-04-16.png",
+      "https://i.ibb.co/rGH49St/Screenshot-2024-10-18-at-18-54-58.png",
   },
   {
-    title: "TROA Makelaardij",
-    link: "https://troa.nl/",
+    title: "Luteo Fliesen",
+    link: "https://luteofliesen.de/",
     thumbnail:
-      "https://i.ibb.co/8mP1qbz/Screenshot-2024-10-16-at-22-54-02.png",
+      "https://i.ibb.co/Wc4Zsvf/Screenshot-2024-10-18-at-18-56-23.png",
   },
   {
-    title: "Heki Made",
-    link: "https://hekimade.com/",
+    title: "Fast & Furious Depannage",
+    link: "https://ff-depannage.ch/",
     thumbnail:
-      "https://i.ibb.co/tsQLY0b/Screenshot-2024-10-16-at-22-59-20.png",
+      "https://i.ibb.co/yQZ59J4/Screenshot-2024-10-18-at-18-57-49.png",
   },
 
 ];
