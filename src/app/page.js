bcb292269260cfd087cf735f626426bdb7d0c200
gabriel-lunesu <g.lunesu@outlook.com>
2024-10-20 14:46:59 +0200

    removed import not using

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 import Testimonials from '@/components/common/Testimonials';
-import AnimatedTestimonials from '@/components/home/Testimonials1';
-import Hero from '@/components/home/Hero';
 import HowItWorks from '@/components/home/HowItWorks';
 import Pricing from '@/components/home/Pricing';
 import CTA from '@/components/home/CTA';
 import SeoCheck from '@/components/home/SeoCheck';
-import HeroNew from '@/components/home/Hero-new';
 import HeroParallax from '@/components/common/HeroParallax';
 export default function Home() {
   return (
