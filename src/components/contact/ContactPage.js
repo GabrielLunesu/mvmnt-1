bff23f2e507410b89aa432b3b1ca0e7cb618d529
gabriel-lunesu <g.lunesu@outlook.com>
2024-10-28 16:05:39 +0100

    contact page copy

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -24,7 +24,7 @@ export default function ContactPage() {
                                 </div>
                                 <div className="flex items-center">
                                     <svg className="w-6 h-6 mr-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /></svg>
-                                    <span>+31 6 12 34 56 78</span>
+                                    <span>085 060 8991</span>
                                 </div>
                             </div>
                             <div className="mt-12">
@@ -45,7 +45,7 @@ export default function ContactPage() {
                         {/* Calendly Widget */}
                         <div className="w-full lg:w-2/3 bg-white p-12">
                             <h2 className="text-3xl font-bold mb-6 text-purple-600">Plan een afspraak</h2>
-                            <div className="h-[500px]">
+                            <div className="h-[600px] ">
                                 <CalendlyEmbed 
                                     url={calendlyUrl}
                                     className="rounded-lg" 
