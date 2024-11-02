1ce6163a0e095279ffaa75929e50f94c34d60c18
gabriel-lunesu <g.lunesu@outlook.com>
2024-11-02 20:07:15 +0100

    soe check colors

--- src/components/home/SeoCheck.jsx
+++ src/components/home/SeoCheck.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function SeoCheck() {
   return (
       <div className=" bg-white  mx-auto px-12 md:px-18">
-        <div className="bg-gradient-to-b from-pink-300 via-pink-700 to-purple-900 rounded-2xl p-2 max-w-5xl mx-auto">
+        <div className="bg-gradient-to-b from-pink-600 via-pink-700 to-purple-900 rounded-2xl p-2 max-w-5xl mx-auto">
           <div className="rounded-3xl bg-white py-16 px-4 w-full" >
             <h1 className="font-heading text-gray-900 text-center text-4xl md:text-5xl font-bold mb-4">Doe de <b>gratis</b> SEO check!</h1>
             <p className="text-gray-600 w-2/3 md:w-1/2 lg:w-1/2 xl:w-1/2 mx-auto text-center mb-10">Klik op de knop hieronder om je website kosteloos, zonder je gegevens achter te hoeven laten op SEO te checken. Zie direct wat niet goed staat, en krijg tips om je website beter te laten presteren.</p>
